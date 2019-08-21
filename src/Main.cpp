40af2e58ca313dd6254d37a865ee9a18c830383b
Tyler Lewis <tlewiscpp@gmail.com>
2019-08-20 21:30:30 -0400

    Auto Commit - chromebook-samus

--- src/Main.cpp
+++ src/Main.cpp
@@ -374,11 +374,11 @@ void globalLogHandler(QtMsgType type, const QMessageLogContext &context, const Q
     }
     //coreLogMessage.erase(std::remove_if(coreLogMessage.begin(), coreLogMessage.end(),[](char c) { return c == '\"'; }), coreLogMessage.end());
     if ((type == QtCriticalMsg) || (type == QtFatalMsg)) {
-        logMessage = QString{"[%1][%2] %3 (%4:%5, %6)"}.arg(QDateTime::currentDateTime().time().toString(), logContext,
+        logMessage = QString{"[%1]-[%2] %3 (%4:%5, %6)"}.arg(QDateTime::currentDateTime().time().toString(), logContext,
                                                              coreLogMessage.c_str(), context.file,
                                                              QS_NUMBER(context.line), context.function);
     } else {
-        logMessage = QString{"[%1][%2] %3"}.arg(QDateTime::currentDateTime().time().toString(), logContext,
+        logMessage = QString{"[%1]-[%2] %3"}.arg(QDateTime::currentDateTime().time().toString(), logContext,
                                                  coreLogMessage.c_str());
     }
     bool addLineEnding{true};
